4f1714788e9ed4121801a237eddfa3cf52a2e4d8
blindauer <e.blindauer@gmail.com>
2016-04-20 10:02:48 +0200

    Ajout setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+import os
+readme = os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+with open(readme) as fh:
+    long_description = fh.read()
+
+setup(name="netmagis-tools",
+      version="0.1",
+      description="CLI for netmagis",
+      long_description=long_description,
+      author="E.Blindauer",
+      author_email="e.blindauer@gmail.com",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[mechanize],
+      )
+
