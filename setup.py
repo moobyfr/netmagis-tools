61586b29532d903b6eec88547dff536a2b0a1079
blindauer <e.blindauer@gmail.com>
2016-04-20 10:36:35 +0200

    Setup working

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from setuptools import find_packages, setup
-
+from distutils.core import setup
 import os
+
 readme = os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
 with open(readme) as fh:
     long_description = fh.read()
 
 setup(name="netmagis-tools",
-      package_dir={'':'scripts'},
+      package_dir={'netmagisclient': 'scripts/netmagisclient'},
+      packages=['netmagisclient'],
       version="0.1",
       description="CLI for netmagis",
       long_description=long_description,
       author="E.Blindauer",
       author_email="e.blindauer@gmail.com",
-      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       )
-
